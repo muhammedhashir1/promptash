ecd9e4c44dfbedf86b79e9786a5c5bb61e4c1242
muhammed hashir <hashiraachi818@gmail.com>
2023-12-22 20:56:17 +0530

    frst cmt

---
+++ utils/database.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // track the connection
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    isConnected = false; // Update isConnected to reflect the failure
+    console.error("MongoDB connection error:", error.message);
+    throw error; // Throw the error to be caught by the calling code
+  }
+};
